b58297e583fdcf41f5f9a4085449eb5a24c9b148
muno17 <munozd17@gmail.com>
2024-05-07 07:57:59 -0400

    add math functions and initial html

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+let first;
+let second;
+let operator;
+
+function operate(x, y, op) {
+    if (op === "+") {
+        return add(x, y);
+    }
+
+    if (op === "-") {
+        return subtract(x, y);
+    }
+
+    if (op === "*") {
+        return multiply(x, y);
+    }
+
+    if (op === "/") {
+        return divide(x, y);
+    }
+}
